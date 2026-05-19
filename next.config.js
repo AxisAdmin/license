04a923225d3297ef89093d873383f0b82fb53fa7
wjjung <wjjung@starplayer.net>
2026-05-19 10:42:45 +0900

    api url 변경

--- next.config.js
+++ next.config.js
@@ -11,9 +11,19 @@ const nextConfig = {
         source: '/spauth',
         destination: '/api/spauth',
       },
+      // /spauth.php?license_code=  →  pages/api/spauth/index.js
+      {
+        source: '/spauth.php',
+        destination: '/api/spauth',
+      },
       // /spauth/spauth?license_code=  →  pages/api/spauth/spauth.js
       {
         source: '/spauth/spauth',
+        destination: '/api/spauth/spauth',
+      },
+      // /spauth/spauth.php?license_code=  →  pages/api/spauth/spauth.js
+      {
+        source: '/spauth/spauth.php',
         destination: '/api/spauth/spauth',
       },
     ];
